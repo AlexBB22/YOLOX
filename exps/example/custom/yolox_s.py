e9329e31bda249da1167595a2d2a857e0e2f69ae
Alexandru Bobe <Alexandru.Bobe@sas.com>
2024-06-28 14:41:57 +0200

    change exps

--- exps/example/custom/yolox_s.py
+++ exps/example/custom/yolox_s.py
@@ -14,11 +14,11 @@ class Exp(MyExp):
         self.exp_name = os.path.split(os.path.realpath(__file__))[1].split(".")[0]
 
         # Define yourself dataset path
-        self.data_dir = "datasets/coco128"
-        self.train_ann = "instances_train2017.json"
-        self.val_ann = "instances_val2017.json"
+        self.data_dir = "/kaggle/input/safety-detection"
+        self.train_ann = "/kaggle/input/safety-detection/train/_annotations.coco.json"
+        self.val_ann = "/kaggle/input/safety-detection/valid/_annotations.coco.json"
 
-        self.num_classes = 71
+        self.num_classes = 5
 
         self.max_epoch = 300
         self.data_num_workers = 4
