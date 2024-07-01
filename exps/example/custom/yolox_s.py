16bf9916d6782596f3218c42206482af2858b0af
Alexandru Bobe <Alexandru.Bobe@sas.com>
2024-07-01 10:23:34 +0200

    try no paths

--- exps/example/custom/yolox_s.py
+++ exps/example/custom/yolox_s.py
@@ -14,13 +14,13 @@ class Exp(MyExp):
         self.exp_name = os.path.split(os.path.realpath(__file__))[1].split(".")[0]
 
         # Define yourself dataset path
-        self.data_dir = "/kaggle/input/d/alexandrubobe2/safety2017"
-        self.train_ann = "/kaggle/input/d/alexandrubobe2/safety2017/train2017/_annotations.coco.json"
-        self.val_ann = "/kaggle/input/d/alexandrubobe2/safety2017/val2017/_annotations.coco.json"
+        # self.data_dir = "/kaggle/input/d/alexandrubobe2/safety2017"
+        # self.train_ann = "/kaggle/input/d/alexandrubobe2/safety2017/train2017/_annotations.coco.json"
+        # self.val_ann = "/kaggle/input/d/alexandrubobe2/safety2017/val2017/_annotations.coco.json"
 
         self.num_classes = 5
 
         self.max_epoch = 300
-        # self.data_num_workers = 4
+        self.data_num_workers = 4
         self.eval_interval = 1
         self.basic_lr_per_img = 0.005 / 64.0
