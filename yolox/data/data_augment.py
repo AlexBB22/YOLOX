0063a95384ccca2350636aab7bbd1df79bd821b5
Alexandru Bobe <Alexandru.Bobe@sas.com>
2024-07-22 14:41:47 +0200

    new training

--- yolox/data/data_augment.py
+++ yolox/data/data_augment.py
@@ -138,44 +138,41 @@ def _mirror(image, boxes, prob=0.5):
         boxes[:, 0::2] = width - boxes[:, 2::-2]
     return image, boxes
 
+def preproc(img, input_size, swap=(2, 0, 1)):
+    if len(img.shape) == 3:
+        padded_img = np.ones((input_size[0], input_size[1], 3), dtype=np.uint8) * 114
+    else:
+        padded_img = np.ones(input_size, dtype=np.uint8) * 114
 
-# def preproc(img, input_size, swap=(2, 0, 1)):
-#     if len(img.shape) == 3:
-#         padded_img = np.ones((input_size[0], input_size[1], 3), dtype=np.uint8) * 114
-#     else:
-#         padded_img = np.ones(input_size, dtype=np.uint8) * 114
+    r = min(input_size[0] / img.shape[0], input_size[1] / img.shape[1])
+    resized_img = cv2.resize(
+        img,
+        (int(img.shape[1] * r), int(img.shape[0] * r)),
+        interpolation=cv2.INTER_LINEAR,
+    ).astype(np.uint8)
+    padded_img[: int(img.shape[0] * r), : int(img.shape[1] * r)] = resized_img
 
-#     r = min(input_size[0] / img.shape[0], input_size[1] / img.shape[1])
-#     resized_img = cv2.resize(
-#         img,
-#         (int(img.shape[1] * r), int(img.shape[0] * r)),
-#         interpolation=cv2.INTER_LINEAR,
-#     ).astype(np.uint8)
-    
-#     padded_img[: int(img.shape[0] * r), : int(img.shape[1] * r)] = resized_img
+    padded_img = padded_img.transpose(swap)
+    padded_img = np.ascontiguousarray(padded_img, dtype=np.float32)
+    return padded_img, r
 
-#     padded_img = padded_img.transpose(swap)
-#     padded_img = np.ascontiguousarray(padded_img, dtype=np.float32)
-#     return padded_img, r
+# def preproc(image, expected_size, swap=(2, 0, 1)):
+#     print("USING LETTERBOX PREPROC")
+#     ih, iw, _ = image.shape
+#     eh, ew = expected_size
+#     scale = min(eh / ih, ew / iw)
+#     nh = int(ih * scale)
+#     nw = int(iw * scale)
 
-
-def preproc(image, expected_size, swap=(2, 0, 1)):
-    print("USING LETTERBOX PREPROC")
-    ih, iw, _ = image.shape
-    eh, ew = expected_size
-    scale = min(eh / ih, ew / iw)
-    nh = int(ih * scale)
-    nw = int(iw * scale)
-
-    image = cv2.resize(image, (nw, nh), interpolation=cv2.INTER_CUBIC)
-    new_img = np.full((eh, ew, 3), 128, dtype='uint8')
-    # fill new image with the resized image and centered it
-    new_img[(eh - nh) // 2:(eh - nh) // 2 + nh,
-            (ew - nw) // 2:(ew - nw) // 2 + nw,
-            :] = image.copy()
-    new_img = new_img.transpose(swap)
-    new_img = np.ascontiguousarray(new_img, dtype=np.uint8)
-    return new_img, scale
+#     image = cv2.resize(image, (nw, nh), interpolation=cv2.INTER_CUBIC)
+#     new_img = np.full((eh, ew, 3), 128, dtype='uint8')
+#     # fill new image with the resized image and centered it
+#     new_img[(eh - nh) // 2:(eh - nh) // 2 + nh,
+#             (ew - nw) // 2:(ew - nw) // 2 + nw,
+#             :] = image.copy()
+#     new_img = new_img.transpose(swap)
+#     new_img = np.ascontiguousarray(new_img, dtype=np.uint8)
+#     return new_img, scale
 
 
 class TrainTransform:
